129c287f5c38b1f6f0fd86700abe27cf6fc2865e
Sudeepvicky <battusudeep33@gmail.com>
2024-04-03 23:33:20 +0530

    second update

--- Testing_1/src/main/java/spring/mvc/Controller/SampleController.java
+++ Testing_1/src/main/java/spring/mvc/Controller/SampleController.java
@@ -10,6 +10,7 @@ public class SampleController {
 	public ModelAndView hello() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("hello");
+		mv.addObject("name","Sudeep");
 		return mv;
 	}
 
